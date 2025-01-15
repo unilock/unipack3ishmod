bd9afa4f0c16632126a70dcd1fdc3b33d900d512
unilock <unilock@fennet.rentals>
2025-01-14 19:17:19 -0500

    DSURROUND LOGGING ARGH
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/unipack/mixin/MusicTrackerMixinSquared.java
@@ -0,0 +1,20 @@
+package cc.unilock.unipack.mixin;
+
+import com.bawnorton.mixinsquared.TargetHandler;
+import net.minecraft.client.sounds.MusicManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = MusicManager.class, priority = 1500)
+public class MusicTrackerMixinSquared {
+	@TargetHandler(
+		mixin = "org.orecruncher.dsurround.mixins.audio.MixinMusicManager",
+		name = "dsurround_startPlaying"
+	)
+	@Redirect(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lorg/orecruncher/dsurround/lib/logging/IModLog;info(Ljava/lang/String;[Ljava/lang/Object;)V", remap = false))
+	private void dsurround_startPlaying(@Coerce Object instance, String msg, Object... params) {
+		// NO-OP
+	}
+}
