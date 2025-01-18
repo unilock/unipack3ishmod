2452cb40363ebab0cec2feb2d41cbba38dd97e84
unilock <unilock@fennet.rentals>
2025-01-18 17:38:39 -0500

    shrink item tooltip
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/unipack/UniPack.java
+++ src/main/java/cc/unilock/unipack/UniPack.java
@@ -1,8 +1,12 @@
 package cc.unilock.unipack;
 
 import com.mojang.logging.LogUtils;
+import net.gigabit101.shrink.items.ShrinkItems;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -18,11 +22,18 @@ public class UniPack {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
 
+        MinecraftForge.EVENT_BUS.addListener(this::itemTooltip);
         MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         LOGGER.info("unilock was here");
+    }
+
+    private void itemTooltip(final ItemTooltipEvent event) {
+        if (event.getItemStack().is(ShrinkItems.SHRINKING_DEVICE.get())) {
+            event.getToolTip().add(Component.literal("doesn't require power").withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC));
+        }
     }
 
     private void registerCommands(final RegisterCommandsEvent event) {
