1f437e03debfcb736815b2752125dba763e5ed7b
unilock <unilock@fennet.rentals>
2025-01-19 08:44:49 -0500

    do things the forge way...
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/unipack/UniPack.java
+++ src/main/java/cc/unilock/unipack/UniPack.java
@@ -4,14 +4,18 @@ import com.mojang.logging.LogUtils;
 import net.gigabit101.shrink.items.ShrinkItems;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
+import java.util.UUID;
 
 @Mod(UniPack.MOD_ID)
 public class UniPack {
@@ -23,6 +27,7 @@ public class UniPack {
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.addListener(this::itemTooltip);
+        MinecraftForge.EVENT_BUS.addListener(this::livingHurt);
         MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
     }
 
@@ -33,6 +38,24 @@ public class UniPack {
     private void itemTooltip(final ItemTooltipEvent event) {
         if (event.getItemStack().is(ShrinkItems.SHRINKING_DEVICE.get())) {
             event.getToolTip().add(Component.literal("doesn't require power").withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC));
+        }
+    }
+
+    private void livingHurt(final LivingHurtEvent event) {
+        if (event.getEntity() instanceof Player defender && event.getSource().getEntity() instanceof Player attacker) {
+            UUID defenderUuid = defender.getUUID();
+            UUID attackerUuid = attacker.getUUID();
+
+            if (defenderUuid.equals(attackerUuid)) {
+                return;
+            }
+
+            boolean defenderPvpDisabled = !PVPManager.pvpWhitelist.isWhiteListed(defender.getGameProfile());
+            boolean attackerPvpDisabled = !PVPManager.pvpWhitelist.isWhiteListed(attacker.getGameProfile());
+
+            if (defenderPvpDisabled || attackerPvpDisabled) {
+                event.setCanceled(true);
+            }
         }
     }
 
