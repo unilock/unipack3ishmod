4d582fe5f76809f261fd02b82b08e7d9ad8acbbf
unilock <unilock@fennet.rentals>
2025-01-24 13:14:50 -0500

    mixin config plugin
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/unipack/UniPackMixin.java
@@ -0,0 +1,50 @@
+package cc.unilock.unipack;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class UniPackMixin implements IMixinConfigPlugin {
+	@Override
+	public void onLoad(String mixinPackage) {
+		// NO-OP
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		// NO-OP
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		String id = mixinClassName.replace("cc.unilock.unipack.mixin.", "").split("\\.")[0];
+
+		return FabricLoader.getInstance().isModLoaded(id);
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+		// NO-OP
+	}
+
+	@Override
+	public List<String> getMixins() {
+		// NO-OP
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+		// NO-OP
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+		// NO-OP
+	}
+}
