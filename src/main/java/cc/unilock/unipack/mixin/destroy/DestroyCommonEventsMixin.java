46aa6f0719fdb99e00c19fd6f522f3ce6fcfe6b2
unilock <unilock@fennet.rentals>
2025-02-15 20:35:59 -0500

    hi i'm destroy i like running code on the client that shouldn't run on the client
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/unipack/mixin/destroy/DestroyCommonEventsMixin.java
@@ -0,0 +1,16 @@
+package cc.unilock.unipack.mixin.destroy;
+
+import com.petrolpark.destroy.events.DestroyCommonEvents;
+import net.minecraftforge.event.TickEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = DestroyCommonEvents.class, remap = false)
+public class DestroyCommonEventsMixin {
+	@Inject(method = "onTick(Lnet/minecraftforge/event/TickEvent$LevelTickEvent;)V", at = @At("HEAD"), cancellable = true)
+	private static void onTick(TickEvent.LevelTickEvent event, CallbackInfo ci) {
+		if (event.side.isClient()) ci.cancel();
+	}
+}
