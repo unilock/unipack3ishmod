eea91a37c071e3512d81bbb1e2d91579b4dbc26c
unilock <unilock@fennet.rentals>
2025-02-17 13:21:48 -0500

    try to fix corail backpack again
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/unipack/mixin/corail_backpack/UtilsMixin.java
+++ src/main/java/cc/unilock/unipack/mixin/corail_backpack/UtilsMixin.java
@@ -4,12 +4,12 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.items.ItemStackHandler;
-import org.cursegame.minecraft.backpack.util.InventoryUtils;
 import org.cursegame.minecraft.backpack.util.Utils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
+import top.theillusivec4.curios.api.type.inventory.IDynamicStackHandler;
 
 import java.util.Optional;
 
@@ -21,6 +21,17 @@ public class UtilsMixin {
 	 */
 	@Overwrite
 	public static Optional<ItemStack> capItem(LivingEntity entity, TagKey<Item> criteria) {
-		return entity.getCapability(CuriosCapability.INVENTORY).resolve().flatMap(curiosItemHandler -> curiosItemHandler.getCurios().values().stream().filter(curiosStacksHandler -> curiosStacksHandler instanceof ItemStackHandler).map(curiosStacksHandler -> (ItemStackHandler) curiosStacksHandler).map(itemStackHandler -> InventoryUtils.getFirstMatching(itemStackHandler, criteria)).findFirst());
+		return CuriosApi.getCuriosInventory(entity).resolve().map(curiosItemHandler -> {
+			for (ICurioStacksHandler curioStacksHandler : curiosItemHandler.getCurios().values()) {
+				IDynamicStackHandler dynamicStackHandler = curioStacksHandler.getStacks();
+				for (int i = 0; i < dynamicStackHandler.getSlots(); i++) {
+					ItemStack stack = dynamicStackHandler.getStackInSlot(i);
+					if (stack.is(criteria)) {
+						return stack;
+					}
+				}
+			}
+			return null;
+		});
 	}
 }
