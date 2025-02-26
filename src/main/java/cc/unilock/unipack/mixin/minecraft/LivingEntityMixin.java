3b0c33458b905d41d18b9ab8ecf0fefdc077edff
unilock <unilock@fennet.rentals>
2025-02-26 13:43:32 -0500

    fix weird crash with spectrum and big brain and saxgknkweiopqwkgjbnlksdsga
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/unipack/mixin/minecraft/LivingEntityMixin.java
@@ -0,0 +1,22 @@
+package cc.unilock.unipack.mixin.minecraft;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Map;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+	@Shadow @Final private Map<MobEffect, MobEffectInstance> activeEffects;
+
+	@WrapMethod(method = "hasEffect(Lnet/minecraft/world/effect/MobEffect;)Z")
+	private boolean hasEffect(MobEffect effect, Operation<Boolean> original) {
+		return this.activeEffects != null && original.call(effect);
+	}
+}
