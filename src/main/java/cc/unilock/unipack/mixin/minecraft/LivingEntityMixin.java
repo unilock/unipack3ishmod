54a7c6e531c16a21cecbeb18d78f091f26b56067
unilock <unilock@fennet.rentals>
2025-02-26 13:51:18 -0500

    why no remappy
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/unipack/mixin/minecraft/LivingEntityMixin.java
+++ src/main/java/cc/unilock/unipack/mixin/minecraft/LivingEntityMixin.java
@@ -1,13 +1,14 @@
 package cc.unilock.unipack.mixin.minecraft;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 
@@ -15,8 +16,8 @@ import java.util.Map;
 public class LivingEntityMixin {
 	@Shadow @Final private Map<MobEffect, MobEffectInstance> activeEffects;
 
-	@WrapMethod(method = "hasEffect(Lnet/minecraft/world/effect/MobEffect;)Z")
-	private boolean hasEffect(MobEffect effect, Operation<Boolean> original) {
-		return this.activeEffects != null && original.call(effect);
+	@Inject(method = "hasEffect(Lnet/minecraft/world/effect/MobEffect;)Z", at = @At("HEAD"), cancellable = true)
+	private void hasEffect(MobEffect effect, CallbackInfoReturnable<Boolean> cir) {
+		if (this.activeEffects == null) cir.setReturnValue(false);
 	}
 }
